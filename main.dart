d5e43cb313b8864e4906378b66fe8a9045ab53d7
TlhokomeloDireko <tdireko16@gmail.com>
2023-02-06 12:45:19 +0200

    remove unused method

--- main.dart
+++ main.dart
@@ -3,48 +3,47 @@ import 'dart:io';
 import 'record.dart';
 
 void main() {
-  final dataLines = File('Sample.csv').readAsLinesSync();
-  dataLines.removeAt(0);
   List<TradeRec> trades = [];
-  // RunThroughFile();
-  trades.addAll(Testing(dataLines));
 
+  // import the Sample.csv file
+  final dataLines = File('Sample.csv').readAsLinesSync();
+
+  // Remove the first row of label
+  dataLines.removeAt(0);
+
+  // Perform data extraction on the file
+  trades.addAll(DataExtraction(dataLines));
+
+  // Run through List<TradeRec> and access the PrintRec() of TradeRec class
   for (TradeRec trade in trades) {
     trade.PrintRec();
   }
 }
 
-List<TradeRec> Testing(var dataLines) {
+List<TradeRec> DataExtraction(var dataLines) {
   List<TradeRec> tradesTaken = [];
 
   for (var line in dataLines) {
+    // Variable to break the date into a list with 3 values for Year, Month and Day
     List<String> dateList;
+
+    // Delimiter
     final values = line.split(';');
 
-    //
-    // Values[2] - entryDate
-    // Break the date into year, month and day
-    //
+    // split the date and store it into the list
     dateList = values[2].split('-');
-    //
-    // Values[5] - tradeOutcome
-    // Remove the $ sign and convert the string to double
-    //
+
+    // Remove the $ sign and trim the white space
     values[5] = values[5].replaceAll(RegExp(r'\$'), ' ');
     values[5] = values[5].replaceAll(RegExp(r','), '.');
     values[5] = values[5].trim();
 
-    //
-    // Values[7] - notes
-    // Replace empty notes entries with No Entry Recorded
-    //
-    if (values[6] == ' ') {
+    // Replace empty notes with No Entry Recorded
+    if (values[6] == '') {
       values[6] = 'No Entry Recorded';
     }
 
-    //
-    // Add TradeRecord to list
-    //
+    // Create a new TradeRec and add it to a List
     tradesTaken.add(new TradeRec(
         tradeNo: int.parse(values[0]),
         pairName: values[1],
@@ -57,23 +56,4 @@ List<TradeRec> Testing(var dataLines) {
   }
 
   return tradesTaken;
-}
-
-void RunThroughFile() {
-  // Import file
-  final lines = File('Sample.csv').readAsLinesSync();
-
-  // Remove the labels
-  lines.removeAt(0);
-
-  for (var line in lines) {
-    // The the delimiter
-    final values = line.split(';');
-    final dateList; // break the date into year, month
-
-    dateList = values[2].split('-');
-
-    // Print Data
-    print(dateList);
-  }
 }
